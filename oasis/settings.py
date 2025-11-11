16c8a0254e467a0132b707c3ac1fb3f5eb0d2539
Marediasoft Developer <marediasoftm10@gmail.com>
2025-11-11 06:49:50 +0500

    S3 fixed1

--- oasis/settings.py
+++ oasis/settings.py
@@ -247,7 +247,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
