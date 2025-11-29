8dbe77283f6d3236e0a89268eeb4555d89473927
Marediasoft Developer <marediasoftm10@gmail.com>
2025-11-30 00:10:55 +0500

    last commit before launch3 user limit exceed

--- app/Views/get_users_list.py
+++ app/Views/get_users_list.py
@@ -15,7 +15,7 @@ from django.db.models import Q
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5                 
+    page_size = 15                 
     page_size_query_param = 'page_size'
     max_page_size = 50
 
