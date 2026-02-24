da0fa125bec23083768ad54900ca83e84d56788f
Marediasoft Developer <marediasoftm10@gmail.com>
2026-02-25 02:40:44 +0500

    settings.py fixed

--- oasis/settings.py
+++ oasis/settings.py
@@ -159,7 +159,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'AUTH_HEADER_TYPES': ('Bearer',),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
