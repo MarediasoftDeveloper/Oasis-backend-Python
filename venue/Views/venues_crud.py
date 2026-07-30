b877958b7c04910099d85e5356fd9f9c0a95a447
MarediasoftDeveloper <devs@marediasoft.com>
2026-07-31 04:05:22 +0500

    venues crud reward has key added

--- venue/Views/venues_crud.py
+++ venue/Views/venues_crud.py
@@ -28,7 +28,6 @@ class Venues_Crud(viewsets.ModelViewSet):
                 or Rewards.objects.filter(venue__id=venue_id, is_ended=False).exists()
             )
     
-
             venue["has_active_raffle_or_reward"] = has_active
 
         return Response(data)
@@ -39,13 +38,13 @@ class Venues_Crud(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(venue)
         data = serializer.data
-
+      
         participating_event_ids = VenuesParticipatingEvents.objects.filter(
             venues=venue.venue
         ).exclude(event__status="completed").values_list("event_id", flat=True).distinct()
-
+        has_reward = Rewards.objects.filter(venue__id=venue.venue.id, is_ended=False).exists()
         events = Events.objects.filter(id__in=participating_event_ids)
 
         data["participating_events"] = EventAppSerializer(events, many=True).data
-
+        data["has_reward"] = has_reward
         return Response(data)
