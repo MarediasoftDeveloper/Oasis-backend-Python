821d8c1a1a404016b504b2a35a1ee0fadd263f16
MarediasoftDeveloper <devs@marediasoft.com>
2026-08-12 02:05:14 +0500

    QR scanner modified 1.2

--- oasis/settings.py
+++ oasis/settings.py
@@ -39,7 +39,7 @@ ssm = boto3.client('ssm', region_name=AWS_S3_REGION_NAME)
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 
-secret_key = ssm.get_parameter( 
+secret_key = ssm.get_parameter(
     Name='/myOasis/DJANGO_SECRET_KEY',
     WithDecryption=True
 )['Parameter']['Value']
@@ -199,6 +199,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'oasis.wsgi.application'
 
+QR_BASE_URL = "https://api.myoasis.co.nz"  # Replace with your actual base URL for QR codes
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
