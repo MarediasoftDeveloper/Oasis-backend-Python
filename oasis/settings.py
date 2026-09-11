e7b3719e5cdc187f1ee4f69a904e301db2f989d7
MarediasoftDeveloper <devs@marediasoft.com>
2026-09-11 22:25:37 +0500

    settings.py changed

--- oasis/settings.py
+++ oasis/settings.py
@@ -165,7 +165,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'oasis.wsgi.application'
 
-QR_BASE_URL = "https://web-production-65f6f.up.railway.app"  # Replace with your actual base URL for QR codes
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
